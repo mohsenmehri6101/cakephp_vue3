36ec75cb3bab2557dc11465f74564fe070a2e8a1
mohsen <mohsen.mehri6101@gmail.com>
2023-06-05 07:58:27 +0330

    456465

--- front/src/App.vue
+++ front/src/App.vue
@@ -38,16 +38,20 @@
             <v-card>
               <v-list lines="two">
                 <v-list-subheader>users</v-list-subheader>
-                <template v-for="n in 6" :key="n">
+                <template v-for="({id,name,family,email,password,created,modify}, index) in users" :key="index">
                   <v-list-item>
                     <template v-slot:prepend>
-                      <v-avatar color="grey-darken-1"></v-avatar>
+                      <v-avatar color="grey-darken-1">
+                        <v-img
+                            :width="200"
+                            aspect-ratio="16/9"
+                            :src="avatarsFake[getRandomInt(0,3)]"
+                        />
+                      </v-avatar>
                     </template>
-
-                    <v-list-item-title>Message {{ n }}</v-list-item-title>
-
+                    <v-list-item-title>{{ `${name} ${family}`}}</v-list-item-title>
                     <v-list-item-subtitle>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil repellendus distinctio similique
+                      {{ email }}
                     </v-list-item-subtitle>
                   </v-list-item>
 
@@ -81,27 +85,41 @@ export default {
     //  Login,
   },
   data: () => ({
-    users: [
-      {
-        id: 'id',
-        name: 'name',
-        family: 'family',
-        email: 'email',
-        password: 'password',
-        created: 'created',
-        modify: 'modify',
+        users: [
+          {
+            id: 'id',
+            name: 'name',
+            family: 'family',
+            email: 'mohsen@gmail.com',
+            password: 'password',
+            created: 'created',
+            modify: 'modify',
+          }
+        ],
+        avatarsFake: [
+          '../public/avatars/1.jpg',
+          '../public/avatars/2.jpg',
+          '../public/avatars/3.jpg',
+          '../public/avatars/4.jpg',
+        ],
+        links_auth: [
+          {icon: 'fa-sign-in-alt', title: 'login', componentName: 'login'},
+          {icon: 'fa-sign-out-alt', title: 'logout', componentName: 'logout'},
+        ],
+        links_when_login: [
+          {icon: 'fa-users', title: 'users-list', componentName: 'list-user'},
+          {icon: 'fa-user-cog', title: 'users-store', componentName: 'created-user'},
+          // {icon: 'fa-user-edit', title: 'user-edit', componentName: 'update-user'},
+          // {icon: 'fa-user-slash', title: 'user-delete', componentName: 'create-user'},
+        ],
       }
-    ],
-    links_auth: [
-      {icon: 'fa-sign-in-alt', title: 'login', componentName: 'login'},
-      {icon: 'fa-sign-out-alt', title: 'logout', componentName: 'logout'},
-    ],
-    links_when_login: [
-      {icon: 'fa-users', title: 'users-list', componentName: 'list-user'},
-      {icon: 'fa-user-cog', title: 'users-store', componentName: 'created-user'},
-      {icon: 'fa-user-edit', title: 'user-edit', componentName: 'update-user'},
-      {icon: 'fa-user-slash', title: 'user-delete', componentName: 'create-user'},
-    ],
-  }),
+  ),
+  methods: {
+    getRandomInt(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+    },
+  },
 }
 </script>
